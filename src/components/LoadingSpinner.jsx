45e6a906e07a2599db74e0abc9604175a125dfa2
SalehAhmed10 <44835364+SalehAhmed10@users.noreply.github.com>
2022-05-18 10:21:20 +0500

    head-filter

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Spinner } from 'react-bootstrap'
-import '../pages/styles/loadingspinner.css'
+import './styles/loadingspinner.css'
 
 export default function LoadingSpinner() {
   return (
