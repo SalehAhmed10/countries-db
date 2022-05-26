9ac9d7c8ea83f9f65062917a3724a29e41ec8184
SalehAhmed10 <44835364+SalehAhmed10@users.noreply.github.com>
2022-05-27 03:27:03 +0500

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
-import { BrowserRouter as Router , Route } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+
 import { useState, useEffect } from 'react';
 import Countries from './components/Countries';
 import Header from './components/Header';
 import Filter from './components/Filter';
 
+
 function App() {
   return (
-    <div className="App">
+      <div>
+        <Header/>
+        <div className="body-style">
+          <Routes>
 
-      <Header/>
-      
-        <div className='body-style'>
-
-        <Filter/>
-        <Countries/>
-
-        </div>
-
-
-
+            
+            
+            <Route exact path="/" element={<><Filter/><Countries/></>}  />
+            
         
- 
-    </div>
+          </Routes>
+
+          </div>
+      </div>
   );
 }
 
 export default App;
+
+{/* <Header/>
+      
+     
+
+<div className='body-style'>
+
+<Filter/>
+<Countries/>
+
+</div>  */}
