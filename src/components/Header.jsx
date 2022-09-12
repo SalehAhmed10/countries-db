7b3014797261a5658c26f27bc5775872e7aaeaa2
Saleh Ahmed <44835364+SalehAhmed10@users.noreply.github.com>
2022-09-13 01:17:19 +0500

    added github ref

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import './styles/header.css'
 import Logo from "../assets/logo.gif"
+import github from '../assets/github.png'
 
 const Header = () => {
   return (
     <div>
-
+      <div className='top-header'> 
+              <a href="https://github.com/SalehAhmed10/countries-db" target="_blank">
+                <img src={github} alt="logo" className='logo'/>   
+              </a>
+        </div>
         <header className='header-main'>
             <Container>
+
               <div className='identity'> 
 
               <img src={Logo} alt="" />
